78ac3e76d0e8185b1d5e2ccbfe3d2b78ba765c58
Panudmet Tammwongsa <3482285+panudetjt@users.noreply.github.com>
2022-07-25 06:20:07 +0700

    feat: add utility fill ticket

---
+++ packages/backend/migrates/fillTicket.ts
@@ -0,0 +1,13 @@
+import { DatabaseInit, Ticket, TicketBuyed } from "../database";
+(async () => {
+  try {
+    console.log("Filling ticket.....");
+    await DatabaseInit();
+    await Ticket.updateMany({}, [{ $set: { remaining: "$availablePerDay" } }]);
+    console.log("Filling ticket done");
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
